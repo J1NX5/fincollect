a04084f0dce1cde8f9bf59db7cf57724273c8df9
j1nx5 <j1nx@posteo.de>
2025-12-11 10:04:38 +0100

    port scraper to oop

--- lib/jobcenter.py
+++ lib/jobcenter.py
@@ -4,17 +4,24 @@ from apscheduler.schedulers.background import BackgroundScheduler
 # from lib.financialmodelingprep import FMP_Collector
 from lib.alphavantage import AV_Collector
 
+
 class Jobcenter:
 
     def __init__(self):
         self.scheduler = BackgroundScheduler()
         self.scheduler.add_job(self._get_earning_report_from_fmp, 'interval', minutes=1)
 
-    def _get_earning_report_from_fmp(self):
-        avColl = AV_Collector()
-        avColl.job()
-        return logging.info("_get_earning_report_from_fmp: wurde ausgeführt!")
-
     def start(self):
         self.scheduler.start()
         return logging.info("Jobcenter hat geöffnet")
+
+    def _start_scraper(self):
+        pass
+    
+
+    # def _get_earning_report_from_fmp(self):
+    #     avColl = AV_Collector()
+    #     avColl.job()
+    #     return logging.info("_get_earning_report_from_fmp: wurde ausgeführt!")
+
+    
---
+++ lib/scraper.py
@@ -0,0 +1,118 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import re
+import pandas as pd
+from datetime import datetime
+import json
+
+
+class FinanceScraper:
+
+    def __init__(self):
+
+    # date_today for cvs name
+        self.date_today = datetime.today().strftime('%Y-%m-%d')
+        self.service = Service('/usr/bin/chromedriver')
+        self.chrome_options = Options()
+        self.chrome_options.binary_location = '/usr/bin/chromium-browser'
+        self.chrome_options.add_argument("--headless")
+        self.chrome_options.add_argument('--user-agent=Mozilla/5.0 (X11; Linux x86_64; rv:138.0) Gecko/20100101 Firefox/138.0')
+        self.chrome_options.page_load_strategy = 'eager'
+        self.driver = webdriver.Chrome(service=self.service, options=self.chrome_options)
+        self.base_url = "https://finance.yahoo.com/calendar/earnings"
+        self.fin_base_url = "https://finance.yahoo.com"
+
+    def scrape(self):
+        self.driver.get(self.base_url)
+        self.driver.execute_script("""
+            var buttons = document.querySelectorAll('button');
+            for(var btn of buttons) {
+                if(btn.textContent.includes('Accept') || btn.textContent.includes('Alle akzeptieren')) {
+                    btn.click(); break;
+                }
+            }
+        """)
+        page_string = BeautifulSoup(self.driver.page_source, 'html.parser').find(class_="total")
+
+        if page_string is None:
+            number_of_pages = "1"
+        else:
+            number_of_pages = page_string.get_text(strip=True).split()[-1]
+
+        # in earning_data is the final result
+        earning_data = dict()
+        for offset in range(0, int(number_of_pages)): 
+            self.driver.get(self.base_url)
+            time.sleep(2)
+            soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+            section = soup.find('section', class_='main yf-93c5lg')
+            links = section.find_all('a', attrs={'href': re.compile(r'/quote/[A-Z]+/')})
+            for l in links:
+                # at this point we need to extract the symbol from the link
+                href = l.get("href")
+
+                # filter symbol
+                parts = [p for p in href.split("/") if p]
+                symbol = parts[-1]
+                print(f'Symbol: {symbol}')
+
+                if href and href.startswith("http"):
+                    pass
+                else:
+                    # print(fin_base_url + href + "financials/")
+                    self.driver.get(self.fin_base_url + href + "financials/")
+                    soup_l2 = BeautifulSoup(self.driver.page_source, 'html.parser')
+                    # print(soup_l2.get_text(strip=True))
+                    self.driver.execute_script("""
+                        var buttons = document.querySelectorAll('button');
+                        for(var btn of buttons) {
+                            if(btn.textContent.includes('Quarterly')){
+                                btn.click(); break;
+                            }
+                        }
+                    """)
+                    section_l2 = soup_l2.find('section', class_='finContainer yf-yuwun0')
+                    row = section_l2.find_all(class_="row")
+                    data = []
+                    for r in row:
+                        column = r.select("div.column")
+                        values = [c.get_text(strip=True) for c in column]
+                        data.append(values)
+                    
+                    #create struct for pandas
+                    header = data[0]
+                    data = data[1:]
+
+                    #create pandasframe
+                    df = pd.DataFrame(data, columns=header)
+
+                    ## filter for data and create new frame -> only comlum 0 and 2
+                    df_filtered = df.iloc[:, [0,2]]
+
+                    #prepare fields for jsons struct
+                    date_from_data = df_filtered.columns[1]
+                    file_name = str(symbol + "_" + self.date_today)
+
+                    # create a struct for json export
+                    data_struct = {
+                        symbol : {
+                            date_from_data : {
+                                df_filtered.iloc[i, 0]: df_filtered.iloc[i, 1] for i in range(len(df_filtered))
+                            }
+                        }
+                    }
+                    #write in file
+                    with open(f'reports/{file_name}', 'w') as f:
+                        json.dump(data_struct, f, indent=4)
+            time.sleep(2) 
+        return driver.quit()
+
+if __name__ == "__main__":
+    fso = FinanceScraper()
+    fso.scrape()
